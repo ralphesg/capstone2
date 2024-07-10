1e8e9df6166b2e5386b34a4990e7b3053bf0178e
esguerraralphchristian <esguerraralphchristian@gmail.com>
2024-07-10 09:30:31 +0800

    updated index and auth

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-index.js
+const express = require("express");
+const mongoose = require("mongoose");
+
+const cors = require("cors");
+
+// [ROUTES]
+
+const userRoutes = require("./routes/user.js");
+const cartRoutes = require("./routes/cart.js");
+const productRoutes = require("./routes/product.js");
+
+// [Environment Setup] 
+
+require("dotenv").config();
+
+// [Server setup] 
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+const corsOptions = {
+	origin: ["http://localhost:8000"],
+	credentials: true,
+	optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
+
+mongoose.connect(process.env.MONGODB_STRING);
+
+mongoose.connection.once('open', () => console.log("Now connected to MongoDB Atlas"));
+
+// [Backend Routes]
+
+app.use("/users", userRoutes);
+app.use("/cart", cartRoutes);
+app.use("/product", productRoutes);
+
+// [Server Gateway Response]
+
+
+if(require.main === module) {
+	app.listen(process.env.PORT || 4000, () => console.log(`API is now available in port ${process.env.PORT || 4000}`));
+}
+
+module.exports = { app, mongoose };
