9232779c655efac37068a9b835b14cbc0d43ac02
Ralph Christian Dionisio Esguerra <esguerraralphchristian@gmail.com>
2024-07-11 00:32:49 +0000

    updated s52 deliverables route

--- routes/product.js
+++ routes/product.js
@@ -10,13 +10,13 @@ router.get("/all", verify, verifyAdmin, productController.getAllProduct);
 
 router.get("/active", productController.getAllActive);
 
-router.get("/:id", productController.getProduct);
+router.get("/:productid", productController.getProduct);
 
-router.patch("/:id/update", verify, verifyAdmin, productController.updateProduct);
+router.patch("/:productid/update", verify, verifyAdmin, productController.updateProduct);
 
-router.patch("/:id/archive", verify, verifyAdmin, productController.archiveProduct);
+router.patch("/:productid/archive", verify, verifyAdmin, productController.archiveProduct);
 
-router.patch("/:id/activate", verify, verifyAdmin, productController.activateProduct);
+router.patch("/:productid/activate", verify, verifyAdmin, productController.activateProduct);
 
 // [SECTION] Export Route System
 module.exports = router;
