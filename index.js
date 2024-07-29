b6efa25154f450cc9ae1e4e17a56dccf0aa8b622
Ralph Esguerra <107236185+ralphesg@users.noreply.github.com>
2024-07-29 11:37:43 +0800

    update corsoptions

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 const corsOptions = {
-	origin: ["http://localhost:5173"],
+	origin: ["https://capstone3-mocha.vercel.app"],
 	credentials: true,
 	optionsSuccessStatus: 200
 }
